1000901ce71b22b497b06685dcbe3409ac3189e9
Phlegx Systems OG <register@phlegx.com>
2014-11-20 19:28:35 +0100

    Add two test users to seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.find_or_create_by!(email: 'user1@example.com') do |u|
+  u.password = 'password'
+  u.password_confirmation = 'password'
+end
+
+User.find_or_create_by!(email: 'user2@example.com') do |u|
+  u.password = 'password'
+  u.password_confirmation = 'password'
+end
